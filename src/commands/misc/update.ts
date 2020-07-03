20c1813b935b4d326d597acd1dbac78a2b980847
Ven <electioxvenomancy@gmail.com>
2020-07-03 19:14:37 +0100

    emojify

--- src/commands/misc/update.ts
+++ src/commands/misc/update.ts
@@ -5,20 +5,20 @@ import { execSync } from 'child_process';
 const ThisCommandOptions: CommandOptions = {
   ...DefaultCommandOptions,
   name: 'update',
-  requiredPermissions: ['ADMINISTRATOR']
+  requiredPermissions: ['ADMINISTRATOR'],
 };
 
 export default class extends Command {
-  constructor (store: CommandStore, file: string[], directory: string) {
+  constructor(store: CommandStore, file: string[], directory: string) {
     super(store, file, directory, ThisCommandOptions);
   }
 
-  public async run (message: KlasaMessage): Promise<KlasaMessage | KlasaMessage[] | null> {
-    const msg = await message.sendMessage('Pulling from Git');
+  public async run(message: KlasaMessage): Promise<KlasaMessage | KlasaMessage[] | null> {
+    const msg = await message.sendMessage('<:github:669562305176141831> Pulling from Git');
     execSync('git pull');
-    await msg.edit('Transpiling');
+    await msg.edit('<:ts:728674877829152889> Transpiling');
     execSync('tsc');
-    await msg.edit('Restarting');
+    await msg.edit('🔄 Restarting');
     process.exit(-1);
   }
 }
