2b9ccc836e0373dfd1819ad96a7ee80fcc746fb6
Ven <electioxvenomancy@gmail.com>
2020-07-03 19:12:37 +0100

    fix

--- src/commands/discord.bio/profile.ts
+++ src/commands/discord.bio/profile.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-irregular-whitespace */
-import { Command, CommandStore, CommandOptions, KlasaMessage } from 'klasa';
+import { Command, CommandStore, CommandOptions, KlasaMessage, KlasaUser } from 'klasa';
 import { MessageEmbed } from 'discord.js';
 import { DefaultCommandOptions } from '../../constants';
 import DiscordBioClient from '../../client';
@@ -7,7 +7,8 @@ import DiscordBioClient from '../../client';
 const ThisCommandOptions: CommandOptions = {
   ...DefaultCommandOptions,
   name: 'profile',
-  aliases: ['p'],
+  aliases: ['p', 'bio'],
+  usage: '[profile:string]',
 };
 
 enum Gender {
@@ -21,45 +22,42 @@ export default class extends Command {
     super(store, file, directory, ThisCommandOptions);
   }
 
-  public async run(message: KlasaMessage): Promise<KlasaMessage | KlasaMessage[] | null> {
-    const content = message.content;
-    if (!content) {
-      return message.sendMessage('No account provided.');
-    } else {
-      const discordBioClient = (<DiscordBioClient>message.client).discordBioClient;
-      const searchParam = message.content.split(' ')[1].replace(/[\\<>@#&!]/g, '');
-      let response;
-      try {
-        response = await discordBioClient.fetchUserDetails(searchParam).then((r) => r.payload);
-      } catch (e) {
-        return await message.sendMessage(e.message);
-      }
-      const { user, discord } = response;
-      const name = `${discord.username}#${discord.discriminator}`;
-      const url = `https://discord.bio/p/${user.details.slug}`;
-      const avatar = discord.avatar
-        ? `https://cdn.discordapp.com/avatars/${discord.id}/${discord.avatar}.png?size=256`
-        : null;
+  public async run(message: KlasaMessage, [profile]: any): Promise<KlasaMessage | KlasaMessage[] | null> {
+    if (!profile) profile = message.author.id;
+    profile = profile.replace(/[\\<>@#&!]/g, '');
 
-      const embed = new MessageEmbed();
-      embed.setColor('PURPLE');
-      embed.setAuthor(name, avatar || undefined, url);
-      if (avatar) embed.setThumbnail(avatar);
-      embed.setTitle(`${name} \`(${user.details.slug})\``);
-      embed.setDescription(
-        `🗒️**About:** ${user.details.description || 'No about set.'}\n​❤️ **${user.details.likes} like${
-          user.details.likes !== 1 ? 's' : ''
-        }**\n​`,
-      );
-      embed.addField('🆔 User ID', discord.id);
-      embed.addField('🗺️ Location', user.details.location || 'No location', true);
-      embed.addField('🎂 Birthday', user.details.birthday ? new Date(user.details.birthday) : 'No birthday', true);
-      embed.addField('🚻 Gender', user.details.gender ? Gender[user.details.gender] : 'No gender', true);
-      embed.addField('✉️ Mail', user.details.email || 'No email', true);
-      embed.addField('🛠️ Occupation', user.details.occupation || 'No occupation', true);
-      embed.addField('🗓️ Account Created', user.details.created_at || 'No creation date.', true);
-
-      return message.send(embed);
+    const discordBioClient = (<DiscordBioClient>message.client).discordBioClient;
+    let response;
+    try {
+      response = await discordBioClient.fetchUserDetails(profile).then((r) => r.payload);
+    } catch (e) {
+      return await message.sendMessage(e.message);
     }
+    const { user, discord } = response;
+    const name = `${discord.username}#${discord.discriminator}`;
+    const url = `https://discord.bio/p/${user.details.slug}`;
+    const avatar = discord.avatar
+      ? `https://cdn.discordapp.com/avatars/${discord.id}/${discord.avatar}.png?size=256`
+      : null;
+
+    const embed = new MessageEmbed();
+    embed.setColor('PURPLE');
+    embed.setAuthor(name, avatar || undefined, url);
+    if (avatar) embed.setThumbnail(avatar);
+    embed.setTitle(`${name} \`(${user.details.slug})\``);
+    embed.setDescription(
+      `🗒️**About:** ${user.details.description || 'No about set.'}\n​❤️ **${user.details.likes} like${
+        user.details.likes !== 1 ? 's' : ''
+      }**\n​`,
+    );
+    embed.addField('🆔 User ID', discord.id);
+    embed.addField('🗺️ Location', user.details.location || 'No location', true);
+    embed.addField('🎂 Birthday', user.details.birthday ? new Date(user.details.birthday) : 'No birthday', true);
+    embed.addField('🚻 Gender', user.details.gender ? Gender[user.details.gender] : 'No gender', true);
+    embed.addField('✉️ Mail', user.details.email || 'No email', true);
+    embed.addField('🛠️ Occupation', user.details.occupation || 'No occupation', true);
+    embed.addField('🗓️ Account Created', user.details.created_at || 'No creation date.', true);
+
+    return message.send(embed);
   }
 }
