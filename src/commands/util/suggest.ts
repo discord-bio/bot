16e639e0ad55ff86b8d8739c30d60bd70b96f5be
Jacher <jwc2002@outlook.com>
2020-08-03 11:40:03 +0100

    make suggest better

--- src/commands/util/suggest.ts
+++ src/commands/util/suggest.ts
@@ -18,6 +18,7 @@ export default class extends Command {
       return message.sendMessage('No suggestion provided.');
     } else {
         const suggestion = content.split(' ').slice(1).join(" ");
+        if(suggestion.trim().length === 0) return message.sendMessage('<:dBoxCross:707177937375461397> You need to provide a suggestion to send.');
         const suggestionChannel = message.client.channels.get(SUGGESTION_CHANNEL_ID);
         if(!suggestionChannel) throw new Error('No channel found');
         return (<KlasaMessage> <unknown> (<TextChannel> suggestionChannel).send('', { embed: {
@@ -35,7 +36,9 @@ export default class extends Command {
               }
             ],
             timestamp: new Date()
-        }}));
+        }}).then(() => {
+          message.sendMessage('<:dBoxCheck:707177937966989353> Your suggestion was submitted.');
+        }));
     }
   }
 }
